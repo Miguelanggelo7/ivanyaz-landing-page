26a458849c4cb38ffba28f182080031ec6d4f353
Jose Saad <josemiguelsaad13@gmail.com>
2022-03-23 18:19:05 -0400

    empezando el banner

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Head from 'next/head'
 import Header from '../components/Header'
 import Footer from '../components/Footer'
+import Banner from '../components/Banner'
 
 export default function Home() {
   return (
@@ -13,6 +14,7 @@ export default function Home() {
 
       <main>
         <Header />
+        <Banner/>
       </main>
 
       <footer>
