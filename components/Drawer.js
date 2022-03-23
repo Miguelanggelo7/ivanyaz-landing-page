11b119c2f4eda097dbf70f3bb41dd7ef986058e9
Jose Saad <josemiguelsaad13@gmail.com>
2022-03-23 17:40:21 -0400

    drawer en header

---
+++ components/Drawer.js
@@ -0,0 +1,59 @@
+import SwipeableDrawer from '@mui/material/SwipeableDrawer';
+import Divider from '@mui/material/Divider';
+import ListItem from '@mui/material/ListItem';
+import styles from '../styles/Drawer.module.css'
+
+export default function Drawer({open, onOpen, onClose, setOpen}) {
+  return (
+    <>
+      <SwipeableDrawer
+        anchor="right"
+        open={open}
+        onOpen={onOpen}
+        onClose={onClose}
+      >
+        <div className={styles.container}>
+          <ListItem
+            button
+            onClick={() => setOpen(false)}
+            className={styles.item}
+          >
+            <p className={styles.textItem}>Sobre mi</p>
+          </ListItem>
+          <ListItem
+            button
+            onClick={() => setOpen(false)}
+            className={styles.item}
+          >
+            <p className={styles.textItem}>Servicios</p>
+          </ListItem>
+          <ListItem
+            button
+            onClick={() => setOpen(false)}
+            className={styles.item}
+          >
+            <p className={styles.textItem}>Portafolios</p>
+          </ListItem>
+          <ListItem
+            button
+            onClick={() => setOpen(false)}
+            className={styles.item}
+          >
+            <p className={styles.textItem}>Testimonios</p>
+          </ListItem>
+          <ListItem
+            button
+            onClick={() => setOpen(false)}
+            className={styles.item}
+          >
+            <p className={styles.textItem}>Mi equipo</p>
+          </ListItem>
+          <Divider style={{margin: '20px'}}/>
+          <button className={styles.button} onClick={() => setOpen(false)}>
+              Asesoría gratuita
+          </button>
+        </div>
+      </SwipeableDrawer>
+    </>
+  )
+}
--- components/Header.js
+++ components/Header.js
@@ -1,8 +1,15 @@
 import styles from '../styles/Header.module.css'
 import { IconButton } from '@mui/material'
 import { Menu } from '@mui/icons-material'
+import { useState } from "react";
+import Drawer from './Drawer';
 
 export default function Header() {
+  const handleDrawer = () => {
+    setOpen(true);
+  };
+  
+  const [open, setOpen] = useState(false);
   return (
     <div className={styles.container}>
       <p className={styles.title}>IvanYaz</p>
@@ -16,11 +23,12 @@ export default function Header() {
             Asesoría gratuita
         </button>
       </div>
-      <div className={styles.menuIconHeader}>
-        <IconButton>
+      <div className={styles.menuIcon}>
+        <IconButton onClick={handleDrawer}>
           <Menu style={{fontSize: "40", color: "#000"}} />
         </IconButton>
       </div>
+      <Drawer open={open} onOpen={() => setOpen(true)} onClose={() => setOpen(false)} setOpen={setOpen}/>
     </div>
   )
 }
